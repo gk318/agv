1088de71131912d68804372563322b7a293d696e
Harshal Dawar <harshaldawar@gmail.com>
2019-01-25 11:05:47 +0100

    trajectory planner works now

--- position_controller/src/trajectory_planner.py
+++ position_controller/src/trajectory_planner.py
@@ -11,25 +11,21 @@ import math
 import rospy
 import tf
 import tf2_ros
-from geometry_msgs.msg import PoseStamped, TransformStamped
+from geometry_msgs.msg import PoseStamped, TransformStamped, PoseArray
 from nav_msgs.msg import Path
-from std_msgs.msg import Float64
+from std_msgs.msg import Float64, Bool
 
 class trajectoryPlanner(object):
 
     def __init__(self, name):
 
         self._name = name
-        self.pub_sp = rospy.Publisher("/move_base/TebLocalPlannerROS/local_plan", Path, queue_size=1)
+        self.pub_sp = rospy.Publisher("/move_base/TebLocalPlannerROS/global_plan", Path, queue_size=1)
 
         self.plan = Path()
-#        self.plan.poses[0].pose.position.x = 0.0
-#        self.plan.poses[0].pose.position.y = 0.0
-#        self.plan.poses[0].pose.position.z = 0.0
-#        self.plan.poses[0].pose.orientation.x = 0.0
-#        self.plan.poses[0].pose.orientation.y = 0.0
-#        self.plan.poses[0].pose.orientation.z = 0.0
-#        self.plan.poses[0].pose.orientation.w = 1.0
+        self.plan.header.frame_id = "agv_base_footprint"
+
+        self.pose = PoseStamped()
 
         rospy.on_shutdown(self.cleanup)
         rospy.loginfo("Start Trajectory planner")
@@ -41,23 +37,26 @@ class trajectoryPlanner(object):
         rospy.loginfo("Stop Trajectory planner")
         rospy.sleep(0.5)
 
-    def setPlan(self, path):
-        print path
-        yaw = path[2] * math.pi / 180.0
-        quat = tf.transformations.quaternion_from_euler(0.0, 0.0, yaw)
+    def poseFromListItem(self, list_item):
+        pose = PoseStamped()
+        sq_yaw = list_item[2] * math.pi / 180.0
+        sq_quat = tf.transformations.quaternion_from_euler(0.0, 0.0, sq_yaw)
 
-        self.plan.poses[0].pose.position.x = path[0] 
-        self.plan.poses[0].pose.position.y = path[1]
-        self.plan.poses[0].pose.position.z = 0.0
-        self.plan.poses[0].pose.orientation.x = quat[0]
-        self.plan.poses[0].pose.orientation.y = quat[1]
-        self.plan.poses[0].pose.orientation.z = quat[2]
-        self.plan.poses[0].pose.orientation.w = quat[3]
+        pose.header.stamp = rospy.Time.now()
+        pose.pose.position.x = list_item[0]
+        pose.pose.position.y = list_item[1]
+        pose.pose.position.z = 0.0
+        pose.pose.orientation.x = sq_quat[0] 
+        pose.pose.orientation.y = sq_quat[1] 
+        pose.pose.orientation.z = sq_quat[2] 
+        pose.pose.orientation.w = sq_quat[3]
+        self.plan.poses.append(pose)
 
-if __name__ == "__main__":
+
+if __name__ == "__main__":        
     
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("--rate", help="Broadcast Rate", type=float, default=5.0)
+    parser.add_argument("--rate", help="Broadcast Rate", type=float, default=1.0)
     parser.add_argument("--points", help="Trajectory points", type=int, default=1)
     args, unknown = parser.parse_known_args()
     
@@ -66,23 +65,14 @@ if __name__ == "__main__":
 
         tp = trajectoryPlanner(rospy.get_name())
         rate = rospy.Rate(args.rate)
-        i = 0
         square = [[3.0,0.0,0.0],[3.0,0.0,90.0],[3.0,3.0,90.0],[3.0,3.0,180.0]]
-
+        abstract = [[0.0,0.0,0.0],[1.0,0.0,0.0],[1.0,0.0,90.0],[1.0,3.0,90.0],[1.0,3.0,0.0],[1.5,3.0,0.0],[1.0,3.0,90.0],[1.0,3.0,90.0],[1.0,3.0,90.0]]
+        for list_item in square:
+            tp.poseFromListItem(list_item)
         while not rospy.is_shutdown():
-            if (i < len(square)):
-                tp.setPlan(square[i])
-                tp.pub_sp.publish(tp.plan)
-            # Debug
-                print i
-                i += 1
-                rate.sleep()
-            else:
-                break
-        #rate.sleep()
-
+            tp.pub_sp.publish(tp.plan)
+            rate.sleep()
 
     except rospy.ROSInterruptException:
         pass
-    # rospy.spin()
-
+    rospy.spin()
